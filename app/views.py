5bd41ffe9a9506d638998217984b6f99c85b09fb
Kartik <agrawalkartik455@gmail.com>
2022-02-13 09:58:18 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
+from .form import CustomerRegistrationForm, CustomerProfileForm
+from django.contrib import messages
+from django.http import JsonResponse
+from django.db.models import Q
+
+
+
+class ProductView(View):
+    def get(self,request):
+        beds = Product.objects.filter(category='B')
+        cabinets = Product.objects.filter(category='C')
+        chairsnseating = Product.objects.filter(category='CS')
+        tables = Product.objects.filter(category='T')
+        return render(request, 'app/home.html', {'beds':beds, 'cabinets':cabinets, 'chairsnseating':chairsnseating, 'tables':tables})
+
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product':product})
+
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect('/cart')
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0.0
+        shipping_amount = 70.0
+        totalamount=0.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        #print(cart_product)
+        if cart_product:
+            for p in cart_product:
+                tempamount = (p.quantity * p.product.discounted_price)
+                amount += tempamount
+                totalamount = amount+shipping_amount
+            return render(request, 'app/addtocart.html', {'carts':cart, 'amount':amount, 'totalamount':totalamount})
+        else:
+            return render(request, 'app/emptycart.html')
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity+=1
+        c.save()
+        amount = 0.0
+        shipping_amount= 70.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.discounted_price)
+            amount += tempamount
+            totalamount = amount + shipping_amount
+
+        data = {
+			'quantity':c.quantity,
+			'amount':amount,
+			'totalamount':amount+shipping_amount
+		}
+        return JsonResponse(data)
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html', {'add':add, 'active':'btn-primary'})
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+def mobile(request):
+ return render(request, 'app/mobile.html')
+
+class CustomerRegistrationView(View):
+ def get(self, request):
+  form = CustomerRegistrationForm()
+  return render(request, 'app/customerregistration.html', {'form':form})
+
+ def post(self, request):
+  form = CustomerRegistrationForm(request.POST)
+  if form.is_valid():
+   messages.success(request, 'Congratulations!! Registered Successfully.')
+   form.save()
+  return render(request, 'app/customerregistration.html', {'form':form})
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html',{'form':form,'active':'btn-primary'})
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user
+            name  = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'Congratulations!! Profile Updated Successfully.')
+        return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
